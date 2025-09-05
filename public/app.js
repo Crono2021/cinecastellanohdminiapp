acb55f9e0c7a491921b2bd0cc94b9fd443c58a19
Crono2021 <66385514+Crono2021@users.noreply.github.com>
2025-09-05 21:18:27 +0200

    Add files via upload

--- public/app.js
+++ public/app.js
@@ -82,7 +82,7 @@ async function load(){
         <img class="poster" src="${imgBase}${item.poster_path || ''}" onerror="this.src='';this.style.background='#222'" />
         <div class="meta">
           <div class="title">${item.title}</div>
-          <div class="year">${item.year || ''}</div>
+          <div class="year">${item.year || ''} ${item.type === 'tv' ? '<span class="serie-badge">SERIE</span>' : ''}</div>
         </div>
       </div>
     `).join('');
@@ -109,7 +109,7 @@ async function load(){
       <img class="poster" src="${imgBase}${item.poster_path || ''}" onerror="this.src='';this.style.background='#222'" />
       <div class="meta">
         <div class="title">${item.title}</div>
-        <div class="year">${item.year || ''}</div>
+        <div class="year">${item.year || ''} ${item.type === 'tv' ? '<span class="serie-badge">SERIE</span>' : ''}</div>
       </div>
     </div>
   `).join('');
