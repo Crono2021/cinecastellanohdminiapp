843eaf8ce04ef0da07187744e4487b02a7bbc4fd
Crono2021 <66385514+Crono2021@users.noreply.github.com>
2025-09-05 21:22:46 +0200

    Add files via upload

--- public/app.js
+++ public/app.js
@@ -78,11 +78,13 @@ async function load(){
 
     grid.innerHTML = slice.map(item => `
       <div class="card" data-id="${item.tmdb_id}">
-        ${item.type === 'tv' ? '<span class="serie-badge">SERIE</span>' : ''}
+        
         <img class="poster" src="${imgBase}${item.poster_path || ''}" onerror="this.src='';this.style.background='#222'" />
         <div class="meta">
           <div class="title">${item.title}</div>
-          <div class="year">${item.year || ''} ${item.type === 'tv' ? '<span class="serie-badge">SERIE</span>' : ''}</div>
+          <div class="year">${item.year || ''} 
+        ${item.type === 'tv' ? '<span class="serie-badge">SERIE</span>' : ''}
+      </div>
         </div>
       </div>
     `).join('');
@@ -109,7 +111,7 @@ async function load(){
       <img class="poster" src="${imgBase}${item.poster_path || ''}" onerror="this.src='';this.style.background='#222'" />
       <div class="meta">
         <div class="title">${item.title}</div>
-        <div class="year">${item.year || ''} ${item.type === 'tv' ? '<span class="serie-badge">SERIE</span>' : ''}</div>
+        <div class="year">${item.year || ''} </div>
       </div>
     </div>
   `).join('');
